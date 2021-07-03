a00759916ea687271e729d3175ce32cd2695a82f
Vinz <ballutvincent@gmail.com>
2021-07-03 17:45:06 +0200

    initialize project

---
+++ js/init.js
@@ -0,0 +1,19 @@
+(function ($) {
+  // function for next slide
+  $('.next').click(function () {
+    $('.carousel').carousel('next');
+  });
+
+  // function for prev slide
+  $('.prev').click(function () {
+    $('.carousel').carousel('prev');
+  });
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    duration: 200,
+  });
+  $(function () {
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
